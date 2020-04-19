55415195486377f9541bc9045cebd4f35d461294
Mohamed Shapan <m7shapan@gmail.com>
2020-04-19 18:02:09 +0200

    Copysign, Dim, Floor, Max, Min, Pow, Pow10, Round, RoundToEven func

--- math.go
+++ math.go
@@ -11,3 +11,48 @@ func Abs(x int64) int64 {
 func Ceil(x float64) int64 {
 	return i64(math.Ceil(x))
 }
+
+// Copysign returns a value with the magnitude of x and the sign of y.
+func Copysign(x, y int64) int64 {
+	return i64(math.Copysign(f64(x), f64(y)))
+}
+
+// Dim returns the maximum of x-y or 0.
+func Dim(x, y int64) int64 {
+	return i64(math.Dim(f64(x), f64(y)))
+}
+
+// Floor returns the greatest integer value less than or equal to x.
+func Floor(x float64) int64 {
+	return i64(math.Floor(x))
+}
+
+// Max returns the larger of x or y.
+func Max(x, y int64) int64 {
+	return i64(math.Max(f64(x), f64(y)))
+}
+
+// Min returns the smaller of x or y.
+func Min(x, y int64) int64 {
+	return i64(math.Min(f64(x), f64(y)))
+}
+
+// Pow returns x**y, the base-x exponential of y.
+func Pow(x, y int64) int64 {
+	return i64(math.Pow(f64(x), f64(y)))
+}
+
+// Pow10 returns 10**n, the base-10 exponential of n.
+func Pow10(n int) int64 {
+	return i64(math.Pow10(n))
+}
+
+// Round returns the nearest integer, rounding half away from zero.
+func Round(x float64) int64 {
+	return i64(math.Round(x))
+}
+
+// RoundToEven returns the nearest integer, rounding ties to even.
+func RoundToEven(x float64) int64 {
+	return i64(math.RoundToEven(x))
+}
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 # mathi
-mathi going to be a wrapper for golang [math](https://golang.org/pkg/math) package but it going to accept or return int64 instead of float64
+mathi going to be a wrapxer for golang [math](https://golang.org/pkg/math) package but it going to accept or return int64 instead of float64
 
 ## supported math func 
 - [x] Abs
 - [x] Ceil
-- [ ] Copysign
-- [ ] Dim
-- [ ] Floor
-- [ ] Max
-- [ ] Min
-- [ ] Pow
-- [ ] Pow10
-- [ ] Round
-- [ ] RoundToEven
+- [x] Copysign
+- [x] Dim
+- [x] Floor
+- [x] Max
+- [x] Min
+- [x] Pow
+- [x] Pow10
+- [x] Round
+- [x] RoundToEven
 
 ## new mathi func
 - [ ] RoundN
